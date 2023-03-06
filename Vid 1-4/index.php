812c0910afb69a77ea666bfb24144f204345c48c
Maisho20 <bahajazz70@gmail.com>
2023-03-06 20:08:13 +0700

    Video 1 - 4

---
+++ Vid 1-4/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo</title>
+</head>
+<body>
+    
+    <h1>
+        <?php
+
+            // Vid 1 --> menjelaskan untuk memilih bahasa yang tepat untuk digunakan sebagai landasan untuk membangun sebuah web site
+            // Vid 2 --> memilih tools (Terminal, database, dan text-editor) yang dianggap pas dengan pengguna
+            // Vid 3 - Hello, Commander
+            echo "Hello, Commander";
+            echo "<br>";
+
+            // variables - Vid 4
+            $greeting = "Hello, ";
+            echo "$greeting World";
+            echo "<br>";
+            // perbedaan " dengan ' jika ingin mengembalikan nilai variabel, maka harus menggunakan " untuk mengembalikan nilai variabel
+            // ' digunakan untuk menampilkan isi dari echo apa adanya
+        ?>
+    </h1>
+    
+</body>
+</html>
